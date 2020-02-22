d67e1bd99b52032b04e016732618b09a9e5493bb
Krasimir Tsonev <krasimir@outset.ws>
2020-02-22 08:07:06 +0200

    Working on the setup

--- setup.js
+++ setup.js
@@ -9,15 +9,64 @@ function fail(message) {
   console.error(`\nᐅ ${message}\n`);
   process.exit(1);
 }
+function copy(from, to, overwrite = true) {
+  if (!fs.existsSync(to) || overwrite) {
+    fs.copyFileSync(from, to);
+    return true;
+  }
+  return false;
+}
+function createFolder(p) {
+  if (!fs.existsSync(p)) {
+    fs.mkdirSync(p);
+  }
+}
 
-const projectRoot = process.cwd();
 const setupRoot = __dirname;
+const projectRoot = process.cwd();
+
+const DEFAULT_PKG_JSON = `{
+  "name": "${path.basename(projectRoot)}"
+}`;
 
 if (!fs.existsSync(`${projectRoot}/package.json`)) {
-  fail(`There is no package.json file in ${projectRoot}.\n  Please create one.`);
+  fs.writeFileSync(`${projectRoot}/package.json`, DEFAULT_PKG_JSON);
+}
+if (!fs.existsSync(`${projectRoot}/.gitignore`)) {
+  fs.writeFileSync(`${projectRoot}/.gitignore`, 'node_modules');
 }
 
 const setupPkg = require(path.resolve(__dirname, 'package.json'));
-console.log(setupPkg);
 const projectPkg = require(`${projectRoot}/package.json`);
 
+projectPkg.devDependencies = {
+  ...projectPkg.devDependencies,
+  ...setupPkg.devDependencies,
+};
+projectPkg.scripts = {
+  ...projectPkg.scripts,
+  ...setupPkg.scripts,
+};
+
+// saving changes in package.json
+fs.writeFileSync(
+  `${projectRoot}/package.json`,
+  JSON.stringify(projectPkg, null, 2)
+);
+
+// copying files
+createFolder(`${projectRoot}/src`);
+createFolder(`${projectRoot}/src/components`);
+createFolder(`${projectRoot}/public`);
+[
+  '.babelrc',
+  '.eslintrc',
+  'tsconfig.json',
+  'webpack.config.js',
+  'webpack.prod.js',
+  'src/index.tsx',
+  'src/components/App.tsx',
+  'public/index.html',
+].forEach(f => {
+  copy(`${setupRoot}/${f}`, `${projectRoot}/${f}`);
+});
